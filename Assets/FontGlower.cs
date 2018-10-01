ef58b1d43f67e952498100aa5406283effb04773
Jakob Schwab <j.schwab@mail.de>
2018-10-01 13:17:38 +0200

    Adds score text that updates on left/right-wall-hit

--- Assets/FontGlower.cs
+++ Assets/FontGlower.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Glower : MonoBehaviour
+public class FontGlower : MonoBehaviour
 {
     public Color baseColor;
     public Color glowColor;
@@ -12,11 +13,14 @@ public class Glower : MonoBehaviour
 
     private AnimationCurve animationCurve;
     private Material material;
+    private Text text;
 
     // Use this for initialization
     void Start()
     {
-        this.material = GetComponent<Renderer>().material;
+        this.text = GetComponent<Text>();
+        this.material = Material.Instantiate(this.text.material);
+        this.text.material = this.material;
         this.animationCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 0));
     }
 
@@ -27,11 +31,12 @@ public class Glower : MonoBehaviour
         float alpha = this.animationCurve.Evaluate(Time.time);
         alpha *= alpha;
 
-        float baseGamma = Mathf.GammaToLinearSpace(0.8f);
+        float baseGamma = Mathf.GammaToLinearSpace(1.0f);
         // Change color
         float emission = baseGamma + this.glowIntensity * alpha;
         Color finalColor =
             Color.Lerp(this.baseColor, this.glowColor, alpha) * Mathf.LinearToGammaSpace(emission);
+
         this.material.SetColor("_EmissionColor", finalColor);
     }
 
--- Assets/MatchState.cs
+++ Assets/MatchState.cs
@@ -1,36 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MatchState : MonoBehaviour
 {
+    public Text scoreLeft;
+    public Text scoreRight;
+
+    private int scoreCountLeft = 0;
+    private int scoreCountRight = 0;
 
     // Use this for initialization
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        this.scoreLeft.text = "2";
+        this.scoreRight.text = "3";
     }
 
     public void HitPaddleLeft()
     {
-
     }
+
     public void HitPaddleRight()
     {
-
     }
+
     public void HitWallLeft()
     {
-
+        this.scoreCountRight += 1;
+        this.scoreRight.text = scoreCountRight.ToString();
+        this.scoreRight.gameObject.SendMessage("StartGlow");
     }
+
     public void HitWallRight()
     {
-
+        this.scoreCountLeft += 1;
+        this.scoreLeft.text = scoreCountLeft.ToString();
+        this.scoreLeft.gameObject.SendMessage("StartGlow");
     }
 }
